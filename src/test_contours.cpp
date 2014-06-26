27c9b25b6aac519d1c02f7ac133b97c2a06f5cff
Gustavo Velasco Hernández <gustavo.velascoh@gmail.com>
2014-06-26 01:52:21 -0500

    Integrated contour finding and dummy tracking

--- src/test_contours.cpp
+++ src/test_contours.cpp
@@ -13,6 +13,8 @@
 using namespace cv;
 using namespace std;
 
+RNG rng(12345);
+
 int main (int argc, char** argv)
 {
 	/* ---- Init code ---- */
@@ -30,7 +32,75 @@ int main (int argc, char** argv)
 	}
 	cout << "OK" << endl;
 	/* ---- Init code ---- */
+	namedWindow("Input",1);
+	namedWindow("Output",1);
 
+	for(;;)
+	{
+		Mat frame;
+
+		vector<vector<Point> > contours;
+		vector<vector<Point> > contours_r;
+		Mat frame_gray, threshold_output;
+		vector<Vec4i> hierarchy;
+		cap >> frame; // get a new frame from camera
+		imshow("Input", frame );
+
+
+		cvtColor( frame, frame_gray, CV_BGR2GRAY );
+
+		/// Detect edges using Threshold
+		threshold( frame_gray, threshold_output, 64, 255, THRESH_BINARY );
+
+		/// Find contours
+		findContours( threshold_output, contours_r, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+		cout << "contours #: " << contours_r.size() << endl;
+
+		for( int i = 0; i < contours_r.size(); i++ )
+		{
+			cout << "Contour " << i << " size: " << contours_r[i].size() << ".";
+			cout << "(" << contours_r[i][0].x << "," << contours_r[i][0].y << ")" << endl;
+
+			if (contours_r[i].size() > 80)
+			{
+				contours.push_back(contours_r[i]);
+			}
+
+		}
+
+		waitKey(0);
+
+		vector<vector<Point> > contours_poly( contours.size() );
+		vector<Rect> boundRect( contours.size() );
+
+		for( int i = 0; i < contours.size(); i++ )
+		{
+			approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+			boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+		//minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+		}
+		cout << "cont size: " << contours.size() << endl;
+		Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
+		for( int i = 0; i< contours.size(); i++ )
+		{
+			Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+			drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+			rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+		}
+
+
+		imshow("Output", drawing );
+
+		//waitKey(0);
+			  //return(0);
+
+
+		if(waitKey(30) >= 0) break;
+
+		//return 0;
+	}
+
+	return 0;
 
 }
 
--- src/vehicle_recognition.cpp
+++ src/vehicle_recognition.cpp
@@ -2,17 +2,23 @@
 #include "opencv2/video/video.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "Bgsub.h"
+#include <cmath>
 
 using namespace cv;
+using namespace std;
+
+RNG rng(12345);
+
 void show_cap_info(VideoCapture cap, std::string);
 void morph_ops(InputArray src, OutputArray dst);
+int find_similar_contour(vector<Point> cont, vector<vector<Point> > cont_vec);
 
 int main(int argc, char** argv)
 {
     int key = 0, crop = 0;
     int save_frames = 0;
-    int gui = 0;
-    int save_video = 1;
+    int gui = 1;
+    int save_video = 0, steps = 0;
     const string NAME = "mask_morph.avi";   // Form the new name with container
 
     VideoCapture cap;
@@ -25,7 +31,16 @@ int main(int argc, char** argv)
     }
     else
     {
-        input = argv[1];
+    	if (!strcmp(argv[1],"-s"))
+    	{
+    		steps = 1;
+    		input = "../media_src/the_video.mp4";
+    		crop = 1;
+    	}
+    	else
+    	{
+    		input = argv[1];
+    	}
     }
     
     cap = VideoCapture(input);
@@ -125,9 +140,26 @@ int main(int argc, char** argv)
     double tp, t_max=0.0, t_min = 10.0, t_avg=0.0;
     Vector<double_t> ex_ts;
     //return 1;
+    Mat bg_mask_1, thr_1, track_image;
+    //Vector of contours
+    vector<vector<Point> > objects;
+    vector<vector<Point> > paths;
+
+    int obj_ind=0, path_ind=0;
+
+
+
     for(;;)
     {
+    	/* Get current frame number */
+    	int curr_frame = cap.get(CV_CAP_PROP_POS_FRAMES);
+		std::stringstream ss;
+		ss << "frame " << curr_frame;
+		std::string fns = ss.str();
+
+		// start timer
     	tp = (double)getTickCount();
+
         Mat frame,  bg_mask, bg_mask_ns, frame_masked, mask_morph, mask_rgb;//, cl_mask, op_mask;
         cap >> frame; // get a new frame from camera
         
@@ -138,21 +170,137 @@ int main(int argc, char** argv)
         //std::cout << frame.cols << "x" << frame.rows << std::endl;
 
         //cvtColor(frame, frame, CV_BGR2YCrCb);
-        cvtColor(frame, frame, CV_BGR2GRAY);
+        //cvtColor(frame, frame, CV_BGR2GRAY);
 
 
         GaussianBlur(frame, frame, Size(5,5), 1.5, 1.5);
         
         // Execute background subtraction and get mask
         bg_sub(frame,bg_mask);
+
+        cout << "SUM: " << sum(bg_mask)[0] << endl;
+
+        if (sum(bg_mask)[0] < 1)
+        	bg_mask_1.copyTo(bg_mask);
+
         // Apply thresholding to omit shadow
         threshold(bg_mask,bg_mask_ns,200,255,0);
+
+
+        //if (curr_frame > 1)
+        //	bitwise_or(bg_mask_ns, bg_mask_1, bg_mask_ns);
 
         morph_ops(bg_mask_ns,mask_morph);
 
         // mask original frame
         frame.copyTo(frame_masked,mask_morph);
         
+        //if (curr_frame > 1)
+        bg_mask_ns.copyTo(bg_mask_1);
+
+
+        // valid contours
+        vector<vector<Point> > contours;
+        // all_contours (raw)
+		vector<vector<Point> > contours_r;
+		Mat frame_gray, threshold_output;
+		vector<Vec4i> hierarchy;
+
+//		cvtColor( mask_morph, frame_gray, CV_BGR2GRAY );
+		mask_morph.copyTo(frame_gray);
+
+		/// Detect edges using Threshold
+		threshold( frame_gray, threshold_output, 64, 255, THRESH_BINARY );
+
+		//threshold_output.copyTo(bg_mask_ns);
+		/// Find contours
+		findContours( threshold_output, contours_r, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+		cout << "contours #: " << contours_r.size() << endl;
+
+		if ((contours_r.size() == 0) && ( curr_frame > 1))
+		{
+			findContours( thr_1, contours_r, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+		}
+		else
+		{
+			threshold_output.copyTo(thr_1);
+		}
+
+		for( int i = 0; i < contours_r.size(); i++ )
+		{
+			if ((arcLength(contours_r[i],true) > 180) && (contourArea(contours_r[i],false) > 400))
+			{
+				contours.push_back(contours_r[i]);
+
+				Moments c_mom = moments(contours_r[i]);
+				double c_x = c_mom.m10/c_mom.m00;
+				double c_y = c_mom.m01/c_mom.m00;
+				Point c_point = Point(c_x,c_y);
+				vector<Point> path;
+				if (objects.size() == 0)
+				{
+					cout << "size 0" << endl;
+					objects.push_back(contours_r[i]);
+					path.push_back(c_point);
+					paths.push_back(path);
+				}
+				else
+				{
+					cout << "size 1" << endl;
+					int s_ind = find_similar_contour(contours_r[i], objects);
+
+					if (s_ind >= 0)
+					{
+						objects[s_ind] = contours_r[i];
+						paths[s_ind].push_back(c_point);
+
+						cout << "path " << paths[s_ind] << endl;
+					}
+					else
+					{
+						objects.push_back(contours_r[i]);
+						path.push_back(c_point);
+						paths.push_back(path);
+					}
+				}
+
+
+
+			}
+
+		}
+
+		vector<vector<Point> > contours_poly( contours.size() );
+		vector<Rect> boundRect( contours.size() );
+
+		for( int i = 0; i < contours.size(); i++ )
+		{
+			cout << "Contour " << i << " size: " << contours[i].size() << ". ";
+								cout << "arc_length: " << arcLength(contours[i],true) << ". ";
+								cout << "area: " << contourArea(contours[i],false) << ". ";
+								cout << "(" << contours[i][0].x << "," << contours[i][0].y << ")" << endl;
+
+			approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+			boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+		//minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+		}
+		cout << "cont size: " << contours.size() << endl;
+		Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
+
+		frame.copyTo(drawing);
+		for( int i = 0; i< contours.size(); i++ )
+		{
+			Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+			drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+			rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+		}
+
+		for( int i = 0; i< paths.size(); i++ )
+		{
+			drawContours( drawing, paths, i, Scalar(0,0,0), 1, 8, vector<Vec4i>(), 0, Point() );
+		}
+
+        // Execution time calculations
         tp = ((double)getTickCount() - tp)/getTickFrequency();
         ex_ts.push_back(tp);
 
@@ -164,10 +312,7 @@ int main(int argc, char** argv)
 
         t_avg += tp;
 
-        int curr_frame = cap.get(CV_CAP_PROP_POS_FRAMES);
-        std::stringstream ss;
-		ss << "frame " << curr_frame;
-		std::string fns = ss.str();
+
 		
 
 
@@ -192,9 +337,6 @@ int main(int argc, char** argv)
         {
 			if(std::find(frames_to_save.begin(), frames_to_save.end(), curr_frame)!=frames_to_save.end())
 			{
-
-
-
 				std::cout << fns << std::endl;
 				std::stringstream ln1, ln2, ln3, com;
 
@@ -233,8 +375,8 @@ int main(int argc, char** argv)
         // ------ Show images ------
         if (gui)
         {
-			imshow("Frame", frame);
-			imshow("Mask", frame_masked);
+			imshow("Frame", bg_mask);
+			imshow("Mask", drawing);
 
 
         //imshow("Closing(Frame & Mask)", frame_masked);
@@ -255,6 +397,9 @@ int main(int argc, char** argv)
 				break;
 			}
         }
+
+        if (steps)
+        	waitKey(0);
 
         if (curr_frame >= cap.get(CV_CAP_PROP_FRAME_COUNT))
 		{
@@ -283,10 +428,53 @@ void show_cap_info(VideoCapture cap, std::string input)
 void morph_ops(InputArray src, OutputArray dst)
 {
 	Mat element = getStructuringElement(MORPH_ELLIPSE, Size(3,3),Point(1,1));
+	Mat element2 = getStructuringElement(MORPH_ELLIPSE, Size(5,5),Point(2,2));
 	Mat temp;
 
 	morphologyEx(src, temp, CV_MOP_OPEN, element, Point(1,1),3);
-	morphologyEx(temp, dst, CV_MOP_CLOSE, element, Point(1,1), 4);
+	morphologyEx(temp, dst, CV_MOP_CLOSE, element2, Point(1,1), 5);
+}
+
+int find_similar_contour(vector<Point> cont, vector<vector<Point> > cont_vec)
+{
+	double c_len = arcLength(cont,true);
+	double c_area = contourArea(cont,false);
+	int ind = 0;
+
+	Moments c_mom = moments(cont);
+	double c_x = c_mom.m10/c_mom.m00;
+	double c_y = c_mom.m01/c_mom.m00;
+
+	cout << "MOMS: " << c_mom.m00 << ", " << c_mom.m01 << ", " << c_mom.m10 << " -> " << c_x << ". " << c_y << endl;
+
+	double olen, oarea;
+
+	vector<double> d_len_v , d_area_v, d_cm_v;
+	double d_len, d_area, d_cm;
+
+	for (vector<vector<Point> >::iterator it = cont_vec.begin() ; it != cont_vec.end(); ++it)
+	{
+		d_len = abs(arcLength(*it,true)-c_len);
+		d_area = abs(contourArea(*it,false)-c_area);
+
+		Moments d_mom = moments(*it);
+		double d_x = abs((d_mom.m10/d_mom.m00) - c_x);
+		double d_y = abs((d_mom.m01/d_mom.m00) - c_y);
+
+		cout << "deltas [" << ind << "]: (" << d_len << ", " << d_area << ")-(" << d_x << ", " << d_y << ")";
+
+		if ((d_x < 16) && (d_y < 16))
+		{
+			cout << " = MATCH" << endl;
+			return ind;
+		}
+		else
+			cout << endl;
+
+		ind++;
+	}
+
+	return -1;
 }
 
 /// Separate the image in 3 places ( B, G and R )
